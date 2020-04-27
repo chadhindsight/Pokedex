c02ef9a022622d05171d202f094c5c403c81bc00
Chad Hinds <chadrickj8@gmail.com>
2020-04-27 12:48:28 -0400

    Convert App component to class in case you need to pass state down as props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      
+class App extends Component {
+  render() {
+    return (
+      <div>
         
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
