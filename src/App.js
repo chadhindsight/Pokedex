31b082ce234c6e805993a99a2a203498cdac7f07
Chad Hinds <chadrickj8@gmail.com>
2020-04-28 16:06:58 -0400

    Update  pagination logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './App.css'
 import PokeList from './components/PokeList';
 import { Col} from 'react-bootstrap';
 import Pagination from 'react-bootstrap/Pagination'
-// import PageItem from 'react-bootstrap/PageItem'
-
+import {Route, Switch} from 'react-router-dom';
 class App extends Component {
   state = {
     pokemon: [],
-    activePage: 0,
+    activePage: 1,
     limit: 40,
     // Every time we change page, we need to update the offset. Might remove this later
     offset: 0,
@@ -17,17 +17,17 @@ class App extends Component {
 
   // Doing func this way sets the 'this' context so you dont need to use a bind method
   loadPokemon = async (url) => {
-
     try {
       let res = await axios.get(url)
-        console.log(res)
+        console.log(res.data.count)
         
-        let pages = Math.round(res.count / this.state.limit)
+        let pages = Math.round(res.data.count / this.state.limit)
         this.setState({
           pokemon: [...res.data.results],
           totalPages: pages,
           count: res.count
         })
+        console.log(this.state.totalPages)
       }
       //Error Handling if request fails
       catch (err) {
@@ -42,19 +42,19 @@ class App extends Component {
 
   //This method fires when a user clicks a different page and returns result of a new call to the related endpoint
 handlePaginationSelect =(page) =>{
-  console.log(page)
+ console.log(this.state.activePage)
   // Fix offset later
-  let offset =this.state.limit * Number(page.target.innerText)
+let offset =this.state.limit * Number(page.target.innerText)
   this.loadPokemon(`${this.props.baseURL}?limit=${this.state.limit}&offset=${offset}`);
 }
 
-// PAGINATION LOGIC
+// PAGINATION LOGIC BASED ON VALUE OF TOTAL PAGES IN STATE OBJECT
  pagination = ()=> {
     let items = [];
-   
-    for (let number = 1; number <= 5; number++) {
+
+    for (let number = 1; number <= this.state.totalPages; number++) {
      items.push(
-       <Pagination.Item key={number}>
+       <Pagination.Item key={number} >
          {number}
        </Pagination.Item>
      );
@@ -82,10 +82,13 @@ handlePaginationSelect =(page) =>{
         <Col sm={8} md={12} smoffset={2} mdoffset={1}>
           <PokeList listOfPokemon={this.state.pokemon} />
         </Col>
-
+          <Switch>
+              {/* <Route exact path={} {...props pokemon={this.state.pokemon}}></Route> */}
+          </Switch>
             <Col sm={12}>
             {this.pagination()}
             </Col>
+            {<h2>pokemon stuff</h2>}
       </div>  
     );
   }
