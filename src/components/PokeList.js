ee4d72e3030fa2e5860fe8275cc34116ddaa2403
Chad Hinds <chadrickj8@gmail.com>
2020-04-28 17:44:48 -0400

    Setup endpoint

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ListGroup, ListGroupItem, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const PokeList = ({ listOfPokemon }) => {
 
     let pokemon = listOfPokemon.map((monster) => {
+        console.log(monster.name)
         return (
             <Col sm={6} md={4} key={monster.name}>
-                <ListGroupItem className='PokeList-item'>{monster.name}</ListGroupItem>
+                <Link to={`:${monster.name}`}><ListGroupItem className='PokeList-item'>{monster.name}</ListGroupItem></Link>
             </Col>
         )
     });
