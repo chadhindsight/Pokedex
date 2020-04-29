06f8e043275511b0eb9a282bb2e1fdb3862eb874
Chad Hinds <chadrickj8@gmail.com>
2020-04-29 14:46:54 -0400

    Log the data from response to console

--- src/components/PokeDetails.js
+++ src/components/PokeDetails.js
@@ -7,16 +7,19 @@ class PokeDetails extends Component {
     // console.log(this.props.pokemon)
     // Displays the information for the selected pokemon
 
-    displayDetails = (selectedPokemon) => {
+    displayDetails = async () => {
         // Get the key word from your URL endpoint and check if it matches a value from the pokemon array
-        // let pokemonId = this.props.match.params.id
         let pokemonId = this.props.match.params.id
         // Get the url that will be used for axios request!
-        let pokeURL = this.props.pokemon.find(pokemon => {
+        let specificPokemon = this.props.pokemon.find(pokemon => {
             return pokemon.name === pokemonId
         })
-        console.log(pokeURL)
-        // // console.log(pokemonId)
+        let response = await axios.get(`${specificPokemon.url}`)
+        
+    }
+
+    componentDidMount() {
+        this.displayDetails()
     }
 
     render() {
