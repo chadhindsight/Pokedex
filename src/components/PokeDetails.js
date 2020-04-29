6618a3ff712dbe21c776663981bb337d9133b195
Chad Hinds <chadrickj8@gmail.com>
2020-04-29 11:12:58 -0400

    Move function that displays info to details component

--- src/components/PokeDetails.js
+++ src/components/PokeDetails.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class PokeDetails extends Component {
+      // might have to make a new request
+        //Use this.props.match.params.find() to target the pokemon you want the information for
+        // console.log(this.props.pokemon)
+    // Displays the information for the selected pokemon
+    displayDetails = (selectedPokemon) => {
+
+    }
     render() {
         return (
             <div>
-                
+                <h2>pokemon stuff!</h2>
             </div>
         );
     }
