59326d61547984716f594851b2e7ecc9cb1be5b4
Chad Hinds <chadrickj8@gmail.com>
2020-04-29 15:50:48 -0400

    Format code and fix spacing

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 import './App.css';
 import PokeDetails from './components/PokeDetails';
 import PokeList from './components/PokeList';
-import { Col} from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import Pagination from 'react-bootstrap/Pagination'
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   state = {
@@ -21,77 +21,80 @@ class App extends Component {
   loadPokemon = async (url) => {
     try {
       let res = await axios.get(url)
-        
-        let pages = Math.round(res.data.count / this.state.limit)
-        this.setState({
-          pokemon: [...res.data.results],
-          totalPages: pages,
-          count: res.count
-        })
-        console.log(this.state.totalPages)
-      }
-      //Error Handling if the request fails
-      catch (err) {
-        console.log(`A ${err} was returned. Something is off :(`) ;
-      }
-    
-  }
-  // Load initial set of pokemon once component renders
-  componentDidMount() {
-    this.loadPokemon(this.props.baseURL) 
-  }
-// Displays the information for the selected pokemon
-selectPokemon = (selectedPokemon) =>{
 
-}
+      let pages = Math.round(res.data.count / this.state.limit)
+      this.setState({
+        pokemon: [...res.data.results],
+        totalPages: pages,
+        count: res.count
+      })
+      console.log(this.state.totalPages)
+      // console.log(this.state.pokemon)
+    }
+    //Error Handling if the request fails
+    catch (err) {
+      console.log(`A ${err} was returned. Something is off :(`);
+    }
+
+  }
+  // Load the initial set of pokemon once component renders
+  componentDidMount() {
+    this.loadPokemon(this.props.baseURL)
+  }
+
 
   //This method fires when a user clicks a different page and returns result of a new call to the related endpoint
-handlePaginationSelect =(page) =>{
-  // Fix offset later
-let offset =this.state.limit * Number(page.target.innerText)
-  this.loadPokemon(`${this.props.baseURL}?limit=${this.state.limit}&offset=${offset}`);
-}
+  handlePaginationSelect = (page) => {
+    // Fix offset later
+    let offset = this.state.limit * Number(page.target.innerText)
+    this.loadPokemon(`${this.props.baseURL}?limit=${this.state.limit}&offset=${offset}`);
+  }
 
-// PAGINATION LOGIC BASED ON VALUE OF TOTAL PAGES IN STATE OBJECT
- pagination = ()=> {
+  // PAGINATION LOGIC BASED ON VALUE OF TOTAL PAGES IN STATE OBJECT
+  pagination = () => {
     let items = [];
-
+    // Once a certain number is passed, return the Ellipsis variant before continuing with numbers again
     for (let number = 1; number <= this.state.totalPages; number++) {
-     items.push(
-       <Pagination.Item key={number} className="buns">
-         {number}
-       </Pagination.Item>
-     );
-   }
+      items.push(
+        <Pagination.Item key={number} className="buns">
+          {number}
+        </Pagination.Item>
+      );
+    }
 
-  return(
-    <div>
-      <Pagination onClick={this.handlePaginationSelect}>{items}
-        <Pagination.Ellipsis />
-        <Pagination.First />
-        <Pagination.Prev />
-        <Pagination.Next />
-        <Pagination.Last />
-      </Pagination>
-    </div>
-  )
- };
+    return (
+      <div>
+        <Pagination onClick={this.handlePaginationSelect}>{items}
+          <Pagination.Ellipsis />
+          <Pagination.First />
+          <Pagination.Prev />
+          <Pagination.Next />
+          <Pagination.Last />
+        </Pagination>
+      </div>
+    )
+  };
 
   render() {
-        if (this.state.pokemon.length === 0) { return '...loading' }
-    
-        return (
-       <div>   
+    if (this.state.pokemon.length === 0) { return '...loading' }
+
+    return (
+      <div>
         <Col sm={8} md={12} smoffset={2} mdoffset={1}>
-          <PokeList listOfPokemon={this.state.pokemon} />
+          {/* <PokeList listOfPokemon={this.state.pokemon} /> */}
         </Col>
-          <Switch>
-              <Route path='/pokemon/:id' component={props => <PokeDetails {...props} pokemon={this.state.pokemon}/>} />
-          </Switch>
-            <Col sm={12}>
-            {this.pagination()}
-            </Col> 
-      </div>  
+        <Switch>
+          <Route exact path='/'>
+            <Col sm={8} md={12} smoffset={2} mdoffset={1}>
+              <PokeList listOfPokemon={this.state.pokemon} />
+            </Col>
+          </Route>
+          <Route exact path='/pokemon/:id' component={props => <PokeDetails {...props} pokemon={this.state.pokemon} />} />
+        </Switch>
+        <Col sm={12}>
+          {this.pagination()}
+        </Col>
+      </div>
     );
   }
 }
