9abc6c9f06e36cef40895c759eb6b1f020e01589
Chad Hinds <chadrickj8@gmail.com>
2020-05-02 16:02:14 -0400

    Determine which attack enemy will use

--- src/components/PokeBattle.js
+++ src/components/PokeBattle.js
@@ -11,18 +11,18 @@ class PokeBattle extends Component {
     }
 
     // Function that calculate if move misses 
-    missRate = () =>{
+    missRate = () => {
         let miss = Math.ceil((Math.random() * 10))
         console.log(miss)
-        return  miss === 1 || miss === 5 ? true : false
+        return miss === 1 || miss === 5 ? true : false
     }
     // Function that calculates damage 
     attack = (e) => {
         let rate = this.missRate()
         // This prevents react from setting the event to a null value... I think
         e.persist()
-        console.log(rate)
-        let damage        
+
+        let damage
         if (rate === true) {
             this.setState({
                 playerTurn: false,
@@ -32,17 +32,16 @@ class PokeBattle extends Component {
             console.log(this.state.charHP)
             return this.state.charHP
         }
-
         else {
-           if(this.state.charHP > 0) {
-               if (e.target.innerText === 'Water Cannon') damage = 20
-               else if (e.target.innerText === 'Water Pulse') damage = 38
-               else if (e.target.innerText === 'Surf') damage = 25
-               else if (e.target.innerText === 'Tackle') damage = 12
-           }
-           else {
-               this.state.blastHP <= 0 ? alert('Battle has concluded. You lost') : alert('Battle has concluded. You Won!')
-           }
+            if (this.state.charHP > 0) {
+                if (e.target.innerText === 'Water Cannon') damage = 20
+                else if (e.target.innerText === 'Water Pulse') damage = 38
+                else if (e.target.innerText === 'Surf') damage = 25
+                else if (e.target.innerText === 'Tackle') damage = 12
+            }
+            else {
+                this.state.blastHP <= 0 ? alert('Battle has concluded. You lost') : alert('Battle has concluded. You Won!')
+            }
             //      else {
             //     alert('Game Over! Play again?')
             //  }
@@ -64,25 +63,50 @@ class PokeBattle extends Component {
         //         damage = 0    
         //             break   
         //    }
-        //    After Button is clicked disable it and then enable it again
+        //    After Button is clicked disable all buttons and then enable them again
         e.target.setAttribute("disabled", "true")
         setTimeout(() => {
             e.target.removeAttribute("disabled")
         }, 2000);
-
         this.setState({
             charHP: this.state.charHP - damage,
         })
 
     }
     // OPPONENT"S TURN
-    opponentAttack = ()=> {
+    opponentAttack = () => {
+        let rate = this.missRate()
 
+        if (rate === true) {
+            this.setState({
+                playerTurn: true,
+                dynamicMessage: 'Attack missed!',
+                charHP: this.state.charHP
+            })
+        }
+        else {
+            const flamethrower = 25;
+            const dragonClaw = 18;
+            // Determine which attack enemy will use
+            let amount = Math.ceil((Math.random() * 2))
+            if (amount === 1) {
+                this.setState({
+                    blastHP: this.state.blastHP - flamethrower
+                })
+            }
+            else {
+                this.setState({
+                    blastHP: this.state.blastHP - dragonClaw
+                })
+            }
+        }
     }
+
+
     render() {
         return (
             <div>
-                <div className="game" >
+                <div className="game" ref={this.buttonRef}>
                     <Opponent hp={this.state.charHP} />
                     <Player hp={this.state.blastHP} />
                 </div>
@@ -91,10 +115,10 @@ class PokeBattle extends Component {
                         What should Blastoise do?
                     </div>
                     <div className="actions">
-                        <button ref={this.myRef} onClick={this.attack}>Water Cannon</button>
-                        <button ref={this.myRef} onClick={this.attack}>Water Pulse</button>
-                        <button ref={this.myRef} onClick={this.attack}>Surf</button>
-                        <button ref={this.myRef} onClick={this.attack}>Tackle</button>
+                        <button className="a-btn" onClick={this.attack}>Water Cannon</button>
+                        <button className="a-btn" onClick={this.attack}>Water Pulse</button>
+                        <button className="a-btn" onClick={this.attack}>Surf</button>
+                        <button className="a-btn" onClick={this.attack}>Tackle</button>
                     </div>
                     <div className="continue">
                         {/* On click button to continue */}
