6fe60c8931f94aedb9be88be4031fa474520b5d3
Chad Hinds <chadrickj8@gmail.com>
2020-05-06 10:28:22 -0400

    Add picture of Pikachu

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,12 @@ const Home = (props) => {
         <div className="home">
            <p className='intro'>
                 Welcome to Pokémon Hub. This is your one stop shop for all things Pokémon releated. Pokémon started as 
-                a series of video games developed by Game Freak and published by Nintendo and The Pokémon Company as part of the Pokémon media franchise. Created by Satoshi Tajiri with assistance from Ken Sugimori, the first games released in 1996 in Japan for the Game Boy, and the main series of role-playing video games (RPGs), also referred as the "core series" by their developers. 
+                a series of video games developed by Game Freak and published by Nintendo and The Pokémon Company as part of the Pokémon media franchise. Created by Satoshi Tajiri with assistance from Ken Sugimori, 
+                the first games released in 1996 in Japan for the Game Boy, and the main series of role-playing video games (RPGs), 
+                also referred to as the "core series" by their developers. 
                 Since 1996, the games have continued to be release on each generation of Nintendo's handhelds and
-                 also branced off into different mediato become  the highest-grossing media franchise of all time. 
-                 This site gives you a platform to celebrate and share your love of Pokémon with others from all around the world.            
+                 also branced off into different forms of media and has since become the highest-grossing media franchise of all time. 
+                 This site gives you a platform to celebrate, express, and share your love of Pokémon with others from across the globe.            
                 </p>
              <div className="how-it-works">   
             <img src="https://image.flaticon.com/icons/svg/528/528098.svg" id="pikachu" alt="pikachu" height="50px" />
